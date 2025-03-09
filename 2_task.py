d8da07887a6683a29ce5e23eecca00ee24ebb23a
Serhii Demydenko <serhii-demydenko@serhii-demydenko.local>
2025-03-10 00:33:43 +0200

    fix quantity length

--- 2_task.py
+++ 2_task.py
@@ -1,11 +1,12 @@
 import random
 
 def get_numbers_ticket(min_value: int, max_value: int, quantity: int) -> list:
-    if not (1 <= min_value <= max_value <= 1000) or not (min_value <= quantity <= max_value):
+    if not (1 <= min_value <= max_value <= 1000) or not (min_value <= quantity <= max_value) or (quantity < max_value):
         return []
     
     return sorted(random.sample(range(min_value, max_value + 1), quantity))
 
 # Приклад використання
-lottery_numbers = get_numbers_ticket(1, 49, 6)
+lottery_numbers1 = get_numbers_ticket(1, 49, 6)
+lottery_numbers = get_numbers_ticket(10, 12, 11)
 print("Ваші лотерейні числа:", lottery_numbers)
